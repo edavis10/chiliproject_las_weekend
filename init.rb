5f7e39e0fa2a5d676763482b81d3ce73315d4bf6
Eric Davis <edavis@littlestreamsoftware.com>
2011-03-21 12:01:43 -0700

    [#3922] Remove My Page links for parent and child accounts

--- init.rb
+++ init.rb
@@ -10,3 +10,15 @@ Redmine::Plugin.register :chiliproject_las_weekend do
 
   requires_redmine :version_or_higher => '1.0.0'
 end
+
+Redmine::MenuManager.map :top_menu do |menu|
+  # Remove and replace My page with one that disappears for Parents and Children
+  menu.delete(:my_page)
+  menu.push(:my_page,
+            { :controller => 'my', :action => 'page' },
+            :if => Proc.new {
+              User.current.logged? &&
+              (!User.current.child? && !User.current.parent?)
+            })
+end
+
--- test/integration/travel_requests_menu_test.rb
+++ test/integration/travel_requests_menu_test.rb
@@ -6,8 +6,17 @@ class TravelRequestsTest < ActionController::IntegrationTest
   end
 
   context "logged in as a student" do
+    setup do
+      @user = generate_child_user(:password => 'test', :password_confirmation => 'test')
+      login_as(@user.login, 'test')
+      visit_home
+    end
+    
     should "see a top menu item for 'My Travel Requests'"
-    should "not see a top menu item for 'My Page'"
+    should "not see a top menu item for 'My Page'" do
+      assert has_no_content?("My page")
+    end
+    
     context "when clicking the 'My Travel Requests' link" do
       should "end up on the issues page"
       should "show open issues"
@@ -16,8 +25,17 @@ class TravelRequestsTest < ActionController::IntegrationTest
   end
   
   context "logged in as a parent" do
+    setup do
+      @user = generate_parent_user(:password => 'test', :password_confirmation => 'test')
+      login_as(@user.login, 'test')
+      visit_home
+    end
+
     should "see a top menu item for 'My Students' Travel'"
-    should "not see a top menu item for 'My Page'"
+    should "not see a top menu item for 'My Page'" do
+      assert has_no_content?("My page")
+    end
+    
     context "when clicking the 'My Students' Travel' link" do
       should "end up on the issues page"
       should "show open issues"
